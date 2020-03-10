e62dfe64ac1f7ff223ae2df5831c27454965f3ef
khotigor <khotigor177@gmail.com>
2020-03-10 19:34:01 +0300

    signalAll added

--- Lab1/discrete.py
+++ Lab1/discrete.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
 import math
+
+import matplotlib.pyplot as plt
+import csv
 from values import *
 
 discrete = plt.figure(dpi=DPI, figsize=(1080 / DPI, 720 / DPI))
@@ -13,14 +15,23 @@ xs = []
 cos_values = []
 
 x = 0.0
-while x <= T_BUILD:
-    cos_values += [
-        AMPLITUDE * math.cos(
-            2 * math.pi * x * FREQUENCY + math.pi * INITIAL_PHASE / 180)
-        + BIAS]
+dataList = [['t', 'u']]
+while x <= T_MATH:
+    new_cos_value = AMPLITUDE * math.cos(
+        2 * math.pi * x * FREQUENCY + math.pi * INITIAL_PHASE / 180) + BIAS
+    cos_values.append(new_cos_value)
+    dataList.append([x, new_cos_value])
     xs += [x]
     x += math.pow(SAMPLING, -1)
 
+pointsFile = open('pointsFile.csv', 'w')
+with pointsFile:
+    writer = csv.writer(pointsFile)
+    writer.writerows(dataList)
+print("Writing complete")
+
 plt.plot(xs, cos_values, '.', label='discrete')
+print("Plotting complete")
 
 discrete.savefig('discrete.png')
+print("Saving complete")
--- Lab1/signalAll.py
+++ Lab1/signalAll.py
@@ -3,9 +3,9 @@ import math
 from values import *
 
 signal = plt.figure(dpi=DPI, figsize=(1080 / DPI, 720 / DPI))
-plt.axis([0, T_BUILD, -1, 2])
+plt.axis([0, T_MATH, -1, 2])
 
-plt.title('Signal')
+plt.title('SignalAll')
 plt.xlabel('t, s')
 plt.ylabel('U, V')
 
@@ -13,7 +13,7 @@ xs = []
 cos_values = []
 
 x = 0.0
-while x <= T_BUILD:
+while x <= T_MATH:
     cos_values += [
         AMPLITUDE * math.cos(
             2 * math.pi * x * FREQUENCY + math.pi * INITIAL_PHASE / 180)
@@ -24,6 +24,8 @@ while x <= T_BUILD:
 plt.plot(xs, cos_values, linestyle='solid', label='Signal')
 print("Plotting complete")
 
-signal.savefig('signal.png')
+signal.savefig('signalAll.png')
 print("Saving complete")
 
+print("Initial phase = {ph}".format(ph=math.pi * INITIAL_PHASE))
+print("Frequency = {fr}".format(fr=FREQUENCY))
